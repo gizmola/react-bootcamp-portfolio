b36bd76ead766cf4348bdc022cd240b14c0cd137
David Rolston <gizmola@gmail.com>
2019-08-11 00:07:29 -0700

    Convert stateless components to function format

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,38 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PROJECTS from '../data/projects';
 import { blockStatement } from '@babel/types';
 
 
-class Project extends Component {
-    render() {
-        console.log('this.props', this.props);
-        const { title, description, link, image} = this.props.project;
-        return (
-            <div style={{ display: 'inline-block', width:300, margin: 10 }}>
-                <h3>{title}</h3>
-                <img src={image} style={{ width:200, height:120 }}/>
-                <p>{description}</p>
-                <a href="{link}" alt="project">{link}</a>
-            </div>
-        );
-    }
+const Project = props => {
+    console.log('props', props);
+    const { title, description, link, image} = props.project;
+    return (
+        <div style={{ display: 'inline-block', width:300, margin: 10 }}>
+            <h3>{title}</h3>
+            <img src={image} style={{ width:200, height:120 }}/>
+            <p>{description}</p>
+            <a href="{link}" alt="project">{link}</a>
+        </div>
+    )    
 }
 
-class Projects extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Highlighted Projects</h2>
-                {
-                    PROJECTS.map(PROJECT => {
-                        return (
-                            <Project key={PROJECT.id} project={PROJECT}/>
-                        )                     
-                    })
-                }
-            </div>
-        )
-    }
-}
+const Projects = () => (
+    <div>
+        <h2>Highlighted Projects</h2>
+        {
+            PROJECTS.map(PROJECT => (
+                <Project key={PROJECT.id} project={PROJECT}/>                 
+            ))
+        }
+    </div>
+ )
 
 export default Projects;
--- src/components/SocialProfile.js
+++ src/components/SocialProfile.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SOCIAL_PROFILES from '../data/socialProfiles.js';
 
-class SocialProfiles extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Connect with me!</h1>
-                {
-                    SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
-                        return (
-                            <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE} />
-                        )    
-                    })
-                }
-            </div>
-        )
-    }
-}
+const SocialProfiles = () => (
+    <div>
+        <h1>Connect with me!</h1>
+        {
+            SOCIAL_PROFILES.map(SOCIAL_PROFILE => (
+                <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE} />
+            ))
+        }
+    </div>
+)   
 
-class SocialProfile extends Component {
-    render() {
-        const { link, image } = this.props.socialProfile; 
-        return (
-            <span>
-                <a key={this.props.key} href={link} alt="social"><img src={image} style={{ width: 35, height: 35, margin: 10 }}/></a>
-            </span>
-        )
-    }
+
+const SocialProfile = props => {
+    const { link, image } = props.socialProfile; 
+    return (
+        <span>
+            <a href={link} alt="social"><img src={image} style={{ width: 35, height: 35, margin: 10 }}/></a>
+        </span>
+    )
 }
 
 export default SocialProfiles;
