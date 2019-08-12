84c88c139db7c40f415204eac3facd8daa487f70
David Rolston <gizmola@gmail.com>
2019-08-11 20:53:29 -0700

    Refactor Joke as stateless component

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+const Joke = ({joke }) => {
+    const { setup, punchline } = joke;
+    return <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+}
+
 class Jokes extends Component {
     state = { joke: {}, jokes: [] };
 
@@ -20,18 +25,11 @@ class Jokes extends Component {
         return (
             <div>
                 <h2>Highlighted Joke</h2>
-                <p>{setup} <em>{punchline}</em></p>
+                <Joke joke={this.state.joke} />     
                 <hr />
                 <h3> Want 10 Random jokes?</h3>
                 <button onClick={this.fetchJokes}>Click Here</button>
-                {
-                    this.state.jokes.map(joke => {
-                        const { id, setup, punchline } = joke;
-                        return (
-                            <p key={id}>{setup} <em>{punchline}</em></p>
-                        )
-                    })
-                }
+                {this.state.jokes.map(joke => (<Joke key={this.state.joke.id} joke={joke} />))}
             </div>
         )
     }
